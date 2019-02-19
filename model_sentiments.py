cde9d19c8d231d51c88c866984168dcedf7ffbca
benyeoh <ben.yeoh@spire.com>
2019-02-19 10:05:22 +0800

    wip

--- model_sentiments.py
+++ model_sentiments.py
@@ -6,7 +6,7 @@ from __future__ import division
 from __future__ import print_function
 
 import os
-os.environ["CUDA_VISIBLE_DEVICES"]="-1"  
+#os.environ["CUDA_VISIBLE_DEVICES"]="-1"  
 
 import collections
 import json
