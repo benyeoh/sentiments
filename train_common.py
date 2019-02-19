4779720ad92b83eda89a85dc154b77190f0524c6
benyeoh <ben.yeoh@spire.com>
2019-02-19 15:03:00 +0800

    wip

--- train_common.py
+++ train_common.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# encoding: utf-8
-
-"""BERT finetuning runner."""
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -12,12 +7,98 @@ import bert.modeling as modeling
 import bert.tokenization as tokenization
 import tensorflow as tf
 
-import model_score
+import model_classifier
 import processors
 import train_common
 
 
-def run(flags):
+# Required parameters
+tf.flags.DEFINE_string(
+    "data_dir", None,
+    "The input data dir. Should contain the .tsv files (or other data files) "
+    "for the task.")
+
+tf.flags.DEFINE_string(
+    "bert_config_file", "bert_models/uncased_L-12_H-768_A-12/bert_config.json",
+    "The config json file corresponding to the pre-trained BERT model. "
+    "This specifies the model architecture.")
+
+tf.flags.DEFINE_string("vocab_file", "bert_models/uncased_L-12_H-768_A-12/vocab.txt",
+                    "The vocabulary file that the BERT model was trained on.")
+
+tf.flags.DEFINE_string(
+    "output_dir", "tmp/sentiments/",
+    "The output directory where the model checkpoints will be written.")
+
+# Other parameters
+
+tf.flags.DEFINE_string(
+    "init_checkpoint", "bert_models/uncased_L-12_H-768_A-12/bert_model.ckpt",
+    "Initial checkpoint (usually from a pre-trained BERT model).")
+
+tf.flags.DEFINE_bool(
+    "do_lower_case", True,
+    "Whether to lower case the input text. Should be True for uncased "
+    "models and False for cased models.")
+
+tf.flags.DEFINE_integer(
+    "max_seq_length", 128,
+    "The maximum total input sequence length after WordPiece tokenization. "
+    "Sequences longer than this will be truncated, and sequences shorter "
+    "than this will be padded.")
+
+tf.flags.DEFINE_integer("train_batch_size", 32, "Total batch size for training.")
+
+tf.flags.DEFINE_integer("eval_batch_size", 8, "Total batch size for eval.")
+
+tf.flags.DEFINE_integer("predict_batch_size", 8, "Total batch size for predict.")
+
+tf.flags.DEFINE_float("learning_rate", 5e-5, "The initial learning rate for Adam.")
+
+tf.flags.DEFINE_float("num_train_epochs", 3.0,
+                   "Total number of training epochs to perform.")
+
+tf.flags.DEFINE_float(
+    "warmup_proportion", 0.1,
+    "Proportion of training to perform linear learning rate warmup for. "
+    "E.g., 0.1 = 10% of training.")
+
+tf.flags.DEFINE_integer("save_checkpoints_steps", 1000,
+                     "How often to save the model checkpoint.")
+
+tf.flags.DEFINE_integer("iterations_per_loop", 1000,
+                     "How many steps to make in each estimator call.")
+
+tf.flags.DEFINE_bool("use_tpu", False, "Whether to use TPU or GPU/CPU.")
+
+tf.flags.DEFINE_string(
+    "tpu_name", None,
+    "The Cloud TPU to use for training. This should be either the name "
+    "used when creating the Cloud TPU, or a grpc://ip.address.of.tpu:8470 "
+    "url.")
+
+tf.flags.DEFINE_string(
+    "tpu_zone", None,
+    "[Optional] GCE zone where the Cloud TPU is located in. If not "
+    "specified, we will attempt to automatically detect the GCE project from "
+    "metadata.")
+
+tf.flags.DEFINE_string(
+    "gcp_project", None,
+    "[Optional] Project name for the Cloud TPU-enabled project. If not "
+    "specified, we will attempt to automatically detect the GCE project from "
+    "metadata.")
+
+tf.flags.DEFINE_string("master", None, "[Optional] TensorFlow master URL.")
+
+tf.flags.DEFINE_integer(
+    "num_tpu_cores", 8,
+    "Only used if `use_tpu` is True. Total number of TPU cores to use.")
+
+tf.flags.DEFINE_bool("use_dropout", True, "Whether to use dropout.")
+
+
+def run_classifier(flags, processor):
     # I'm lazy
     class AttrDict(dict):
         def __init__(self, *args, **kwargs):
@@ -25,7 +106,7 @@ def run(flags):
             self.__dict__ = self
             
     FLAGS = AttrDict()
-    FLAGS.update(flags)
+    FLAGS.update(flags)    
 
     tf.logging.set_verbosity(tf.logging.INFO)
 
@@ -46,8 +127,8 @@ def run(flags):
             (FLAGS.max_seq_length, bert_config.max_position_embeddings))
 
     tf.gfile.MakeDirs(FLAGS.output_dir)
-
-    processor = processors.FiQAProcessor(FLAGS.data_dir)
+    
+    label_list = processor.get_labels()
 
     tokenizer = tokenization.FullTokenizer(
         vocab_file=FLAGS.vocab_file, do_lower_case=FLAGS.do_lower_case)
@@ -72,14 +153,16 @@ def run(flags):
     num_train_steps = None
     num_warmup_steps = None
 
-    train_examples = processor.get_train_examples()
+    train_examples = processor.get_train_examples(FLAGS.data_dir)
     #train_examples = train_examples[:1]
+
     num_train_steps = int(
         len(train_examples) / FLAGS.train_batch_size * FLAGS.num_train_epochs)
     num_warmup_steps = int(num_train_steps * FLAGS.warmup_proportion)
 
-    model_fn = model_score.model_fn_builder(
+    model_fn = model_classifier.model_fn_builder(
         bert_config=bert_config,
+        num_labels=len(label_list),
         init_checkpoint=FLAGS.init_checkpoint,
         learning_rate=FLAGS.learning_rate,
         num_train_steps=num_train_steps,
@@ -98,21 +181,21 @@ def run(flags):
         predict_batch_size=FLAGS.predict_batch_size)
 
     train_file = os.path.join(FLAGS.output_dir, "train.tf_record")
-    model_score.file_based_convert_examples_to_features(
-        train_examples, FLAGS.max_seq_length, tokenizer, train_file)
+    model_classifier.file_based_convert_examples_to_features(
+        train_examples, label_list, FLAGS.max_seq_length, tokenizer, train_file)
 
     tf.logging.info("***** Running training *****")
     tf.logging.info("  Num examples = %d", len(train_examples))
     tf.logging.info("  Batch size = %d", FLAGS.train_batch_size)
     tf.logging.info("  Num steps = %d", num_train_steps)
-    train_input_fn = model_score.file_based_input_fn_builder(
+    train_input_fn = model_classifier.file_based_input_fn_builder(
         input_file=train_file,
         seq_length=FLAGS.max_seq_length,
         is_training=True,
         drop_remainder=True)
     estimator.train(input_fn=train_input_fn, max_steps=num_train_steps)
 
-    eval_examples = processor.get_eval_examples()
+    eval_examples = processor.get_dev_examples(FLAGS.data_dir)
     num_actual_eval_examples = len(eval_examples)
     if FLAGS.use_tpu:
         # TPU requires a fixed batch size for all batches, therefore the number
@@ -121,11 +204,11 @@ def run(flags):
         # later on. These do NOT count towards the metric (all tf.metrics
         # support a per-instance weight, and these get a weight of 0.0).
         while len(eval_examples) % FLAGS.eval_batch_size != 0:
-            eval_examples.append(model_score.PaddingInputExample())
+            eval_examples.append(model_classifier.PaddingInputExample())
 
     eval_file = os.path.join(FLAGS.output_dir, "eval.tf_record")
-    model_score.file_based_convert_examples_to_features(
-        eval_examples, FLAGS.max_seq_length, tokenizer, eval_file)
+    model_classifier.file_based_convert_examples_to_features(
+        eval_examples, label_list, FLAGS.max_seq_length, tokenizer, eval_file)
 
     tf.logging.info("***** Running evaluation *****")
     tf.logging.info("  Num examples = %d (%d actual, %d padding)",
@@ -142,7 +225,7 @@ def run(flags):
         eval_steps = int(len(eval_examples) // FLAGS.eval_batch_size)
 
     eval_drop_remainder = True if FLAGS.use_tpu else False
-    eval_input_fn = model_score.file_based_input_fn_builder(
+    eval_input_fn = model_classifier.file_based_input_fn_builder(
         input_file=eval_file,
         seq_length=FLAGS.max_seq_length,
         is_training=False,
@@ -156,63 +239,4 @@ def run(flags):
         for key in sorted(result.keys()):
             tf.logging.info("  %s = %s", key, str(result[key]))
             writer.write("%s = %s\n" % (key, str(result[key])))
-
-    '''
-    if FLAGS.do_predict:
-        predict_examples = processor.get_test_examples(FLAGS.data_dir)
-        num_actual_predict_examples = len(predict_examples)
-        if FLAGS.use_tpu:
-            # TPU requires a fixed batch size for all batches, therefore the number
-            # of examples must be a multiple of the batch size, or else examples
-            # will get dropped. So we pad with fake examples which are ignored
-            # later on.
-            while len(predict_examples) % FLAGS.predict_batch_size != 0:
-                predict_examples.append(model_score.PaddingInputExample())
-
-        predict_file = os.path.join(FLAGS.output_dir, "predict.tf_record")
-        model_score.file_based_convert_examples_to_features(predict_examples,
-                                                FLAGS.max_seq_length, tokenizer,
-                                                predict_file)
-
-        tf.logging.info("***** Running prediction*****")
-        tf.logging.info("  Num examples = %d (%d actual, %d padding)",
-                        len(predict_examples), num_actual_predict_examples,
-                        len(predict_examples) - num_actual_predict_examples)
-        tf.logging.info("  Batch size = %d", FLAGS.predict_batch_size)
-
-        predict_drop_remainder = True if FLAGS.use_tpu else False
-        predict_input_fn = ufile_based_input_fn_builder(
-            input_file=predict_file,
-            seq_length=FLAGS.max_seq_length,
-            is_training=False,
-            drop_remainder=predict_drop_remainder)
-
-        result = estimator.predict(input_fn=predict_input_fn)
-
-        output_predict_file = os.path.join(FLAGS.output_dir, "test_results.tsv")
-        with tf.gfile.GFile(output_predict_file, "w") as writer:
-            num_written_lines = 0
-            tf.logging.info("***** Predict results *****")
-            for (i, prediction) in enumerate(result):
-                probabilities = prediction["probabilities"]
-                if i >= num_actual_predict_examples:
-                    break
-                output_line = "\t".join(
-                    str(class_probability)
-                    for class_probability in probabilities) + "\n"
-                writer.write(output_line)
-                num_written_lines += 1
-        assert num_written_lines == num_actual_predict_examples
-    '''
-
-
-def main(_):
-    run(tf.app.flags.FLAGS.flag_values_dict())
-
-
-if __name__ == "__main__":
-    tf.flags.mark_flag_as_required("data_dir")
-    # tf.flags.mark_flag_as_required("vocab_file")
-    # tf.flags.mark_flag_as_required("bert_config_file")
-    # tf.flags.mark_flag_as_required("output_dir")
-    tf.app.run()
+    
