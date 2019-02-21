ccb6c2f799eba3906b5eee0aca9b2d5fffbc3894
benyeoh <ben.yeoh@spire.com>
2019-02-22 01:01:43 +0800

    wip

---
+++ train_finpb.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""BERT finetuning runner."""
+
+import processors
+import train_common
+
+import tensorflow as tf
+
+
+def run(flags):
+    processor = processors.FinPBProcessor(flags["data_dir"])
+    train_common.run_classifier(flags, processor)
+
+
+def main(_):
+    run(tf.app.flags.FLAGS.flag_values_dict())
+
+
+if __name__ == "__main__":
+    tf.flags.mark_flag_as_required("data_dir")
+    # tf.flags.mark_flag_as_required("vocab_file")
+    # tf.flags.mark_flag_as_required("bert_config_file")
+    # tf.flags.mark_flag_as_required("output_dir")
+    tf.app.run()
